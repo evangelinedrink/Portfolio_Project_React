b7f193df1b8709fe9a30adab3bd06194d869003b
Evangeline Drink <evangelinedrink@gmail.com>
2021-03-23 23:39:31 -0700

    Card

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
 import logo from './logo.svg';
+import React, {Component} from "react";
+import {Jumbotron, NavBar, Button, Container, Card, CardText, CardBody} from "reactstrap";
+import Icon from "./iconPic.jpg";
 import './App.css';
 
-function App() {
+class App extends Component {
+  render() {
+    return(
+      <div>
+        <Jumbotron fluid>
+          <Container fluid>
+            <div className="inlineJumbotron">
+            {/*Icon Image*/}
+            <img src={Icon} className="iconPicture" alt="Colored pencils arranged in a circle."/>
+            </div>
+
+            <div className="inlineJumbotron">
+            <Card>
+              <CardBody>
+                <CardText>Window displaying new email messages.</CardText>
+              </CardBody>
+            </Card>
+            </div>
+          </Container>
+        </Jumbotron>
+      </div>
+    );
+  }
+}
+//Weather App:  https://www.freecodecamp.org/news/learn-react-by-building-a-weather-app/ 
+
+{/*function App() {
   return (
     <div className="App">
       <header className="App-header">
@@ -20,6 +49,6 @@ function App() {
       </header>
     </div>
   );
-}
+}*/}
 
 export default App;
