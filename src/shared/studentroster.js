bfd8c5d45eca9f5f8b35da521379bdaaea5c777a
Evangeline Drink <evangelinedrink@gmail.com>
2021-03-24 23:23:52 -0700

    Student Roster

---
+++ src/shared/studentroster.js
@@ -0,0 +1,167 @@
+export const STUDENTS = [
+    {
+        id: 0,
+        lastName: "Anderson",
+        firstName: "Mary",
+    },
+    {
+        id: 1,
+        lastName: "Brakerman",
+        firstName: "Deandra",
+    },
+    {
+        id: 2,
+        lastName: "Diaz",
+        firstName: "Manuel",
+    },
+    {
+        id: 3,
+        lastName: "Lee",
+        firstName: "Daniel",
+    },
+    {
+        id: 4,
+        lastName: "Villard",
+        firstName: "Quentin",
+    },
+    {
+        id: 5,
+        lastName: "Holbrook",
+        firstName: "Mayola",
+    },
+    {
+        id: 6,
+        lastName: "Siegel",
+        firstName: "Christel",
+    },
+    {
+        id: 7,
+        lastName: "Borrego",
+        firstName: "Joseph",
+    },
+    {
+        id: 8,
+        lastName: "Osteen",
+        firstName: "Lissette",
+    },
+    {
+        id: 9,
+        lastName: "Gillette",
+        firstName: "Hillary",
+    },
+    {
+        id: 10,
+        lastName: "Noble",
+        firstName: "Matteo",
+    },
+    {
+        id: 11,
+        lastName: "Fortune",
+        firstName: "Samuel",
+    },
+    {
+        id: 12,
+        lastName: "Elston",
+        firstName: "Laurene",
+    },
+    {
+        id: 13,
+        lastName: "McNulty",
+        firstName: "Ahmed",
+    },
+    {
+        id: 14,
+        lastName: "Ramsey",
+        firstName: "Narcisca",
+    },
+    {
+        id: 15,
+        lastName: "Crouch",
+        firstName: "Grant",
+    },
+    {
+        id: 16,
+        lastName: "Neil",
+        firstName: "Vanita",
+    },
+    {
+        id: 17,
+        lastName: "Schiller",
+        firstName: "Enoch",
+    },
+    {
+        id: 18,
+        lastName: "Lenz",
+        firstName: "Shawanna",
+    },
+    {
+        id: 19,
+        lastName: "Richardson",
+        firstName: "Ulysses",
+    },
+    {
+        id: 20,
+        lastName: "Davis",
+        firstName: "Easton",
+    },
+    {
+        id: 21,
+        lastName: "Drinkwater",
+        firstName: "Atticus",
+    },
+    {
+        id: 22,
+        lastName: "Carlyle",
+        firstName: "Claude",
+    },
+    {
+        id: 23,
+        lastName: "Ginty",
+        firstName: "Elidia",
+    },
+    {
+        id: 24,
+        lastName: "Brent",
+        firstName: "Marcelo",
+    },
+    {
+        id: 25,
+        lastName: "Peston",
+        firstName: "Brock",
+    },
+    {
+        id: 26,
+        lastName: "Zane",
+        firstName: "Ash",
+    },
+    {
+        id: 27,
+        lastName: "Delacruz",
+        firstName: "Silas",
+    },
+    {
+        id: 28,
+        lastName: "Hoyle",
+        firstName: "Olivia",
+    },
+    {
+        id: 29,
+        lastName: "Townes",
+        firstName: "Hiram",
+    },
+    {
+        id: 30,
+        lastName: "Nielsen",
+        firstName: "Aleksey",
+    },
+    {
+        id: 31,
+        lastName: "Grimm",
+        firstName: "Nicholas",
+    },
+    {
+        id: 32,
+        lastName: "Couture",
+        firstName: "Nisha",
+    },
+];
