195be943e0121806906603e513da24576b30d089
Evangeline Drink <evangelinedrink@gmail.com>
2021-05-05 21:03:29 -0700

    Announcements in Array and Displayed in the component

--- src/components/announcementComponent.js
+++ src/components/announcementComponent.js
@@ -1,30 +1,35 @@
 import React, {Component} from "react";
 import {Card, CardBody, Button, CardTitle, CardText} from "reactstrap";
+import ANNOUNCEMENTS from "../shared/Announcements.js";
 
 class Announcements extends Component {
     constructor(props){
         super(props);
         this.state= {
+            announcements: ANNOUNCEMENTS,
             selectedAnnouncement: null, //When the user selects an announcement, this value of null will change.
         }
     }
 
     //Method that will run when the user selects an announcement.
-    onAnnouncementSelect(announcements){
-        this.setState({selectedAnnouncement: announcements}) //this.setState will change the value of the selected announcement's property of state. 
-    }
+   // onAnnouncementSelect(announcements){
+  //      this.setState({selectedAnnouncement: announcements}) //this.setState will change the value of the selected announcement's property of state. 
+ //   }
+
     render(){
         return(
             <div>
-                <Card>
-                    <CardBody>
-                        <CardTitle>May 5, 2021: This is the first announcement for Chemistry class.</CardTitle>
-                        <CardText>Make sure to finish the Balancing Chemical Equations activity before Friday, May 7. We will be going over the answers in class.</CardText>
-                        <CardText>
-                            <small className="text-muted">Date this announcement was posted: {Date()} </small>
-                        </CardText>
-                    </CardBody>
-                </Card>
+                {/*Use the map array method to go through the ANNOUNCEMENTS array and create a card for each element in the array. If you don't do this map() method on each element of the array, the announcements will not show up.*/}
+                {this.state.announcements.map(announcement=>
+                    <Card key={announcement.id}> {/*Always have a key attribute (id) for any element in an array. This key will always be placed on the topmost element in each array item.*/}
+                        <CardBody>
+                            <CardTitle><b>{announcement.title}</b></CardTitle> {/*The <b> makes the text bolded. */}
+                            <CardText>{announcement.body}</CardText>
+                            <CardText>
+                                <small className="text-muted">{announcement.date}</small>
+                            </CardText>
+                        </CardBody>
+                    </Card>)}
             </div>
         )
     };
--- src/shared/Announcements.js
+++ src/shared/Announcements.js
@@ -2,14 +2,16 @@
 export const ANNOUNCEMENTS= [
     {
         id:0,
-        announcementTitle: "How to create an announcement on this webpage.",
-        announcementBody: "To create an announcement, click on the button that says `Create a new announcement.` A new module will appear where you can type in the title and text of what you'd like to say to students. Once posted, the announcement will show up with the date when it was posted.",  
-        announcementDate: "Posted on Wednesday, May 5, 2021, at 8:23 pm.",
+        title: "How to create an announcement on this webpage.",
+        body: "To create an announcement, click on the button that says \"Create a new announcement.\" A new module will appear where you can type in the title and text of what you'd like to say to students. Once posted, the announcement will show up with the date when it was posted.",  
+        date: "Posted on Wednesday, May 5, 2021, at 8:23 pm.",
     },
     {
         id: 1,
-        announcementTitle: "May 5, 2021: This is the first announcement for Chemistry class.",
-        announcementBody: "Make sure to finish the Balancing Chemical Equations activity before Friday, May 7. We will be going over the answers in class.",
-        announcementDate: "Posted on Wednesday, May 5, 2021, at 8:24 pm.",
+        title: "May 5, 2021: This is the first announcement for Chemistry class.",
+        body: "Make sure to finish the Balancing Chemical Equations activity before Friday, May 7. We will be going over the answers in class.",
+        date: "Posted on Wednesday, May 5, 2021, at 8:24 pm.",
     },
 ]
+
+export default ANNOUNCEMENTS;
