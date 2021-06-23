a5383c241ba1034b74eb0d90b047fbde7457993d
Evangeline Drink <evangelinedrink@gmail.com>
2021-06-22 23:54:49 -0700

    Installing Weather Icons

--- src/components/weatherAppComponent.js
+++ src/components/weatherAppComponent.js
@@ -1,6 +1,6 @@
 import React, {Component} from "react"; //Importing React
 import {Jumbotron, Navbar, NavbarBrand, NavBarToggler, Collapse, NavLink, Nav, NavItem, Button, Container, Card, CardText, CardBody} from "reactstrap";
-
+import "weather-icons/css/weather-icons.css"; //Importing the Weather Icons
 
 const Weather= () => {
     //We will return JSX
@@ -9,7 +9,10 @@ const Weather= () => {
        <div className="container">
            <div className= "cards">
                 <h1>Weather App</h1>
-                
+                <h5 className= "py-4"> {/*p stands for padding, y stands for top and bottom. 4 is the size. https://mdbootstrap.com/docs/react/utilities/spacing/ */}
+                    {/*Using the Weather Icons. From this website: https://github.com/erikflowers/weather-icons */}
+                    <i className="wi wi-day-sunny"></i>
+                </h5>
            </div> 
        </div>
 
