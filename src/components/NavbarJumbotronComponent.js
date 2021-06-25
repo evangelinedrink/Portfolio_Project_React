7c03bd144a9e2b901d37b2616198da88a9162f29
Evangeline Drink <evangelinedrink@gmail.com>
2021-06-24 20:59:56 -0700

    Attempt to Fetch Data

--- src/components/NavbarJumbotronComponent.js
+++ src/components/NavbarJumbotronComponent.js
@@ -4,24 +4,104 @@ import {Jumbotron, Navbar, NavbarBrand, NavBarToggler, Collapse, NavLink, Nav, N
 import Icon from "../iconPic.jpg";
 import ReactCalendar from "../components/HomeComponent.js";
 import Weather from "../components/weatherAppComponent.js"; //Importing the Weather constant
-import weatherAPIKey from "../bin/weatherAPIKey"; //Importing the Weather API Key
+import weatherAPIKey from "../bin/weatherAPIKey.js"; //Importing the Weather API Key
 
+//https://www.robinwieruch.de/react-fetching-data
+//https://medium.com/@joelazarz/using-concurrently-with-json-server-and-your-react-app-3d07487acc50 
+class WeatherCall extends Component {
+  constructor() {
+    super();
+    this.state= {
+      isLoading: true,
+      weatherData: null
+    };
+  }
 
+  fetchData= () => {
+    const APICall= `http://api.openweathermap.org/data/2.5/weather?q=London,uk@appid=${weatherAPIKey}` //Fetch function will make the API call to get data from OpenWeather. The method to get data from OpenWeather is from this website: https://openweathermap.org/current
+
+    //How to Fetch Data in React and to see Error Handling Examples: https://www.robinwieruch.de/react-fetching-data
+    fetch(APICall)
+      .then(response => {
+          if (response.ok) {
+            return response.json();
+            console.log(response.json());
+          } else {
+            throw new Error("Unable to get data from the server.");
+          }
+      })
+      .then(data => this.setState({weatherData: data, isLoading:false }))
+      .catch(error => this.setState({error, isLoading: false}));
+  }
+
+  componentDidMount= () => {
+    if (this.props.city && this.props.country) {
+      this.fetchData()
+    }
+  }
+
+  render() {
+    return (
+      
+      <div><Weather/></div>
+    )
+  }
+}
+
+/*
 //Need to make a request (API call) from OpenWeather to get data from the API
 class weatherAPICall extends Component {
   constructor() {
     super();
     this.state= {
+      information: [],
       city: undefined, //The city will be placed by the user.
       country: undefined, //The country will be placed by the user.
     };
-    this.getWeather(); //Caling getWeather method/function in the constructor
+    //this.getWeather(); //Caling getWeather method/function in the constructor
   }
 
+  componentDidMount() {
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=London,uk@appid=${weatherAPIKey}`)
+    .then((response) => response.json())
+    .then(data=> {
+      this.setState({information: data})
+    });
+  }
+
+  render() {
+    return(
+      this.state.information.map((data)=> (<li>{data}</li>))
+      
+    )
+  }
+
+  /*
   //Creating a method to fetch data from the OpenWeather API
   getWeather= async ()=> {
+    
+  const weatherURL= `http://api.openweathermap.org/data/2.5/weather?q=London,uk@appid=${weatherAPIKey}` //Fetch function will make the API call to get data from OpenWeather. The method to get data from OpenWeather is from this website: https://openweathermap.org/current
+    
+    /*
+    //Fetching from the Open Weather API
+    componentDidMount= () => {
+      const APICall= `http://api.openweathermap.org/data/2.5/weather?q=London,uk@appid=${weatherAPIKey}` //Fetch function will make the API call to get data from OpenWeather. The method to get data from OpenWeather is from this website: https://openweathermap.org/current
+
+      fetch(APICall)
+        .then(response=> response.json()) //Converting all the data received from OpenWeather to .JSON format
+        .then(data=> console.log("Data List Loaded", data.list))
+
+      }
+      */
+    
+    /*
     const APICall= await fetch(`http://api.openweathermap.org/data/2.5/weather?q=London,uk@appid=${weatherAPIKey}`) //Fetch function will make the API call to get data from OpenWeather. The method to get data from OpenWeather is from this website: https://openweathermap.org/current
   
+    this.componentDidMount()
+      if (!this.state.data) {
+        console.log(this.fetchData());
+      }
+    
     //Converting all the data received from OpenWeather to .JSON format
     const response= await APICall.json();
 
@@ -33,8 +113,12 @@ class weatherAPICall extends Component {
       city: response.name, //Obtained from the response's json file that shows the city under the title name.
       country: response.sys.country, //Obtained from the response's json file that shows the country under the sys tab and then country label.
     })
+    
   }
+  
+
 };
+*/
 
 
 //Classes are syntax sugar for functions.
@@ -54,8 +138,9 @@ class NavbarJumbotron extends Component {
                 <div className="inlineJumbotron col-8">
                 <Card>
                   <CardBody>
-                    
-                    <Weather city={this.state.city} country={this.state.country}/> {/*Calling the Weather App to be displayed in the Card's Body. The city is defined by the user and is a state prop from the constructor in the WeatherAPICall class.*/}
+                    <Weather/>
+
+                    {/*<Weather city={this.state.city} country={this.state.country}/> {/*Calling the Weather App to be displayed in the Card's Body. The city is defined by the user and is a state prop from the constructor in the WeatherAPICall class.*/}
                     
                   </CardBody>
                 </Card>
